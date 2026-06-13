913f6c2ba80e424b4f6f89af31a0f0261bda39ff
Val <darsuev@gmail.com>
2026-06-13 23:18:39 +0200

    logs, какие-то комменты добавил

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -13,8 +13,8 @@
 #ifndef CONFIGPARSER_HPP
 #define CONFIGPARSER_HPP
 
-#include "Config.hpp" //парсер должен создавать структуры.
-#include "ConfigTokenizer.hpp"// парсер потребляет токены.
+#include "Config.hpp"
+#include "ConfigTokenizer.hpp"
 #include <string>
 #include <vector>
 
@@ -23,16 +23,13 @@ class	ConfigParser
 public:
 	explicit ConfigParser(const std::string &path);
 
-	Config	parseConfig();// превращает файл конфига в Config.
-	//Контракт:
-	//на успехе возвращает заполненный Config
-	//на ошибке кидает std::runtime_error с line/col
+	Config	parseConfig();
 
 private:
-	Tokenizer			tokenizer_; //Объект токенайзера: читает файл и выдаёт токены по запросу.
-	Tokenizer::Token	nextToken_; //“Текущий токен” (тот, на который мы сейчас смотрим), lookahead, следующий непрочитанный токен
+	Tokenizer			tokenizer_;
+	Tokenizer::Token	nextToken_;
 
-	void				consumeToken(); // двигает поток токенов. НЕ ПРОВЕРЯЕТ, просто двигает.
+	void				consumeToken();
 	void				expect(Tokenizer::TokenType t, const char *description);
 	bool				isWord(const char *w) const;
 
@@ -40,11 +37,10 @@ private:
 	ServerConfig		parseServer();
 	LocationConfig		parseLocation();
 
-	//Директива — это одна команда/настройка в конфиге, которая заканчивается ";"
 	// Parse directives
 	void						parseServerDirective(ServerConfig &srv);
 	void						parseLocationDirective(LocationConfig &loc);
-	std::vector<std::string>	readArgsUntilSemi();//стандартная терминология: ";" == semicolon->Semi
+	std::vector<std::string>	readArgsUntilSemi();// ";" == semicolon->Semi
 
 	void						applyServerDirective(const Tokenizer::Token &nameTok,
 									const std::vector<std::string> &args,
@@ -53,13 +49,5 @@ private:
 									const std::vector<std::string> &args,
 									LocationConfig &loc);
 };
-/* nextToken_ ВСЕГДА = следующий токен, который ещё не обработан.
-
-Что гарантирует:
-
-expect() проверяет текущий nextToken_, затем делает consumeToken()
-consumeToken() — единственный способ двигаться вперёд
-любая функция, когда заканчивает работу, оставляет nextToken_ на правильном “следующем” месте */
-
 
 #endif
